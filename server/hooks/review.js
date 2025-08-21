562938e6106c213bcbc2d4d7ac14753b8e088a36
ushirayura <yura08052007@gmail.com>
2025-08-22 00:18:46 +0300

    hooks deleted

--- server/hooks/review.js
+++
@@ -1,7 +0,0 @@
-const { updateProductRating } = require('../services/rating.service');
-
-module.exports = (Review) => {
-  Review.afterCreate(updateProductRating);
-  Review.afterUpdate(updateProductRating);
-  Review.afterDestroy(updateProductRating);
-};
