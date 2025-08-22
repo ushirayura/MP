5359af7937ed48999ffe4f62d3335998d0833651
ushirayura <yura08052007@gmail.com>
2025-08-22 17:59:01 +0300

    Update rentRouter.js

--- server/routes/rentRouter.js
+++ server/routes/rentRouter.js
@@ -7,5 +7,7 @@ router.use(authMiddleware);
 router.post('/create', rentController.create);
 router.get('/getAll', rentController.getAll);
 router.put('/:id/status', rentController.updateStatus);
+router.get('/pending', rentController.getPending);
+
 
 module.exports = router;
