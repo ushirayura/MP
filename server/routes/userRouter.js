69b975e84b6e18e9645eee324cb210a92007ba1f
ushirayura <yura08052007@gmail.com>
2025-08-22 16:34:24 +0300

    Update userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,15 +3,12 @@ const router = new Router()
 const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware');
 
-
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', authMiddleware, userController.check)
 router.get('/profile', authMiddleware, userController.getProfile)
 router.put('/updateProfile',authMiddleware, userController.updateProfile);
 router.put('/updatePassword',authMiddleware, userController.updatePassword);
-
-
 
 
 module.exports = router
