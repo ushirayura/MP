e4e266c2c12c2f23861cd5217e1e79297a49edc2
ushirayura <yura08052007@gmail.com>
2025-08-22 16:21:44 +0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,13 @@ const userRouter = require('./userRouter')
 const productRouter = require('./productRouter')
 const rentRouter = require('./rentRouter')
 const favouriteRouter = require('./favouriteRouter')
+const reviewRouter = require('./reviewRouter')
 
 router.use('/user', userRouter)
 router.use('/product', productRouter)
 router.use('/rent', rentRouter)
 router.use('/favourite', favouriteRouter)
+router.use('/review', reviewRouter)
+
 
 module.exports = router
